8939e605212198c5b498a7e179b3686906bb8723
Edward Tejeda <edward.tejeda1987@gmail.com>
2022-04-28 15:45:20 -0400

    Proyecto Banda Musicas

--- js/musica.js
+++ js/musica.js
@@ -15,11 +15,36 @@ function Resultado()  {
     logo.innerHTML = "";
     logo.appendChild(logo1);
 
-    const nombre1 = document.getElementById("h2");
-    nombre1.textContent = artista.artists[0].strArtist;
+    const nombre1 = document.createElement("h1");
+    nombre1.innerHTML = artista.artists[0].strArtist;
     const nombre = document.getElementById("nombreBanda");
     nombre.innerHTML = "";
     nombre.appendChild(nombre1);
+
+    const estilo = document.createElement("h4");
+    estilo.innerHTML = artista.artists[0].strStyle;
+    const estilo1 = document.getElementById("estiloBanda");
+    estilo1.innerHTML = "";
+    estilo1.appendChild(estilo);
+
+    const genero = document.createElement("h4");
+    genero.innerHTML = artista.artists[0].strGenre;
+    const genero1 = document.getElementById("generoBanda");
+    genero1.innerHTML = "";
+    genero1.appendChild(genero);
+
+    const pagina = document.createElement("a");
+    pagina.innerHTML = artista.artists[0].strWebsite;
+    const pagina1 = document.getElementById("paginaBanda");
+    pagina1.innerHTML = "";
+    pagina1.appendChild(pagina);
+
+    const biografia = document.createElement("p");
+    biografia.innerHTML = artista.artists[0].strBiographyES;
+    const biografia1 = document.getElementById("biografiaBanda");
+    biografia1.innerHTML = "";
+    biografia1.appendChild(biografia);
+
 }
 
 function buscar()   {
