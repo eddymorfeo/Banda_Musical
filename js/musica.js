5d877d4178ef8ebf84289d7c7832e1b481d14e19
Edward Tejeda <edward.tejeda1987@gmail.com>
2022-04-28 23:20:04 -0400

    Proyecto Banda Musical

--- js/musica.js
+++ js/musica.js
@@ -2,15 +2,14 @@ function Resultado()  {
     const artista = JSON.parse(this.responseText);
 
     const img = document.createElement("img");
-    img.src = artista.artists[0].strArtistBanner;
+    img.src = artista.artists[0].strArtistThumb;
     img.style = "width:100%";
     const imagen = document.getElementById("imagen");
     imagen.innerHTML = "";
     imagen.appendChild(img);
 
     const logo1 = document.createElement("img");
-    logo1.src = artista.artists[0].strArtistLogo;
-    logo1.style = "width:50%"
+    logo1.src = artista.artists[0].strArtistLogo;    
     const logo = document.getElementById("logo");
     logo.innerHTML = "";
     logo.appendChild(logo1);
@@ -40,17 +39,80 @@ function Resultado()  {
     pagina1.appendChild(pagina);
 
     const biografia = document.createElement("p");
-    biografia.innerHTML = artista.artists[0].strBiographyES;
+    biografia.innerHTML = artista.artists[0].strBiographyEN;
     const biografia1 = document.getElementById("biografiaBanda");
     biografia1.innerHTML = "";
-    biografia1.appendChild(biografia);
-
+    biografia1.appendChild(biografia);   
+    
 }
+
+
+/*function Discografia() {
+    const albumes = JSON.parse(this.responseText);    
+    
+    const discos = document.createElement("li");
+    discos.innerHTML = albumes.album[0].strAlbum;
+    const dis = document.getElementById("discografia");
+    dis.innerHTML = "";
+    dis.appendChild(discos); 
+
+    const anio = document.createElement("label");
+    anio.innerHTML = albumes.album[0].intYearReleased;
+    const anioDisco = document.getElementById("anio");
+    anioDisco.innerHTML = "";
+    anioDisco.appendChild(anio); 
+    
+}*/
+
+
+function Discografia() {
+    const artista = JSON.parse(this.responseText);
+    let discos = document.getElementById("discografiaBanda");
+    discos.innerHTML = "";
+    artista.album.forEach(alb => {
+        const fila = document.createElement("div")
+        fila.className = "row"
+        const colAnio = document.createElement("div")
+        colAnio.className = "col-1"
+        colAnio.innerHTML = alb.intYearReleased
+        const colDisco = document.createElement("div")
+        colDisco.className = "col-4"
+        colDisco.innerHTML = alb.strAlbum
+        fila.appendChild(colAnio)
+        fila.appendChild(colDisco)
+        discos.appendChild(fila)
+    });
+}
+
+
+
+/*function Discografia()   {
+    let discos = document.getElementById("discografia");
+    discos.innerHTML = "";
+    personas.forEach(pers => {
+        const fila = document.createElement("div")
+        fila.className = "row"
+            const colNombre = document.createElement("div")
+            colNombre.className = "col-4"
+            colNombre.innerHTML = pers.nombre
+            const colApellido = document.createElement("div")
+            colApellido.className = "col-4"
+            colApellido.innerHTML = pers.apellido
+        fila.appendChild(colNombre)
+        fila.appendChild(colApellido)
+        elementoPersonas.appendChild(fila)
+    });
+}*/
 
 function buscar()   {
     const condicion = document.getElementById("condicion").value
-    const request = new XMLHttpRequest();
+    const condicion2 = document.getElementById("condicion").value
+    const request = new XMLHttpRequest();    
     request.addEventListener('load', Resultado);
-    request.open('GET', 'https://www.theaudiodb.com/api/v1/json/2/search.php?s=' + condicion);   
-    request.send();
+    const request2 = new XMLHttpRequest();
+    request2.addEventListener('load', Discografia);
+    request.open('GET', 'https://www.theaudiodb.com/api/v1/json/2/search.php?s=' + condicion); 
+    request.send();      
+    request2.open('GET', 'https://theaudiodb.com/api/v1/json/2/discography.php?s=' + condicion2);    
+    request2.send();
 }
